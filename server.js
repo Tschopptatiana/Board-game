2ff43dcf5493bdd4b281767db9dd35e6782dbb95
Anna Gubskaya <anna.gubskaya@dif.tech>
2025-03-03 01:56:02 +0300

    update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import { Server } from "socket.io";
 import { v4 as uuidv4 } from "uuid";
 import fs from "fs";
 import dotenv from "dotenv";
-import { saveRoomsToSupabase, loadRoomsFromSupabase } from "./ supabase";
+import { saveRoomsToSupabase, loadRoomsFromSupabase } from "./supabase";
 
 
 dotenv.config();
