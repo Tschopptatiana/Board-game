298cf2a5848b6908698d4385be311f6bae669543
Anna Gubskaya <anna.gubskaya@dif.tech>
2025-03-03 03:03:46 +0300

    update sup

--- supabase.js
+++ supabase.js
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
+import dotenv from "dotenv";
 
-const SUPABASE_URL = process.env.SUPABASE_URL;
+// Загружаем переменные окружения
+dotenv.config();
+
+
 const SUPABASE_KEY = process.env.SUPABASE_KEY;
 
 if (!SUPABASE_URL || !SUPABASE_KEY) {
